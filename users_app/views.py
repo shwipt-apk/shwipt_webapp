61928fb3a28d768258f71663c133e094f10e10d8
Dhiransh Saxena <dhiranshsaxena@Dhiranshs-MacBook-Pro.local>
2023-09-05 03:26:15 +0530

    Version 1.0.0

--- users_app/views.py
+++ users_app/views.py
@@ -4,26 +4,10 @@ from django.http import JsonResponse
 import json
 import datetime
 from django.views.decorators.csrf import csrf_exempt
-from firebase_admin import credentials, initialize_app
-import os
 
 # Setting up the flask app
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '123456789qwert'
-
-creds = credentials.Certificate({
-    "type": os.environ.get("type"),
-    "project_id": os.environ.get("project_id"),
-    "private_key_id": os.environ.get("private_key_id"),
-    "private_key": os.environ.get("private_key").replace('\n', '\n'),
-    "client_email": os.environ.get("client_email"),
-    "client_id": os.environ.get("client_id"),
-    "auth_uri": os.environ.get("auth_uri"),
-    "token_uri": os.environ.get("token_uri"),
-    "auth_provider_x509_cert_url": os.environ.get("auth_provider_x509_cert_url"),
-    "client_x509_cert_url": os.environ.get("client_x509_cert_url")
-})
-default_app = initialize_app(creds)
 
 # Setting up theFirebase Database
 db = firestore.client()
