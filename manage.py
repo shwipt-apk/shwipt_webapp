5da1e3a314e7697e38efb0f6ad02383752db6772
Dhiransh Saxena <dhiranshsaxena@Dhiranshs-MacBook-Pro.local>
2023-09-05 02:52:41 +0530

    Version 1.0.0

--- manage.py
+++ manage.py
@@ -2,11 +2,26 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from firebase_admin import credentials, initialize_app
 
+creds = credentials.Certificate({
+    "type": os.environ.get("type"),
+    "project_id": os.environ.get("project_id"),
+    "private_key_id": os.environ.get("private_key_id"),
+    "private_key": os.environ.get("private_key").replace('\n', '\n'),
+    "client_email": os.environ.get("client_email"),
+    "client_id": os.environ.get("client_id"),
+    "auth_uri": os.environ.get("auth_uri"),
+    "token_uri": os.environ.get("token_uri"),
+    "auth_provider_x509_cert_url": os.environ.get("auth_provider_x509_cert_url"),
+    "client_x509_cert_url": os.environ.get("client_x509_cert_url")
+})
+
+default_app = initialize_app(creds)
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shwipt_app.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
